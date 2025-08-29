061db051ac724b1d09bfe57192d764036ef74659
Piotr Makuch <piotr.makuch19@gmial.com>
2025-08-29 22:57:31 +0200

    feat: add random background color switch

--- GradebookApp.js
+++ GradebookApp.js
@@ -1,10 +1,11 @@
+const body = document.querySelector('body');
 const scoreAdd = document.querySelector(".score-add-form");
 const subjectDropdown = document.querySelector("#subject_dropdown"); 
 const classInput = document.querySelector("#class_number_entry");
 const nameInput = document.querySelector("#name");
 const surnameInput = document.querySelector("#surname");
 const assessmentDropdown = document.querySelector("#assessments_dropdown");
-
+const switchBtn = document.getElementById('bgr_switch_btn');
 
 
 
@@ -19,9 +20,9 @@ let scores = [];
 function getAverageScore(scores) {
   let sumScores = 0;
   for(const scoresCounter of scores) {
-
+    
     sumScores = sumScores + scoresCounter;
-
+    
   }
   return sumScores / scores.length;
   
@@ -34,7 +35,7 @@ console.log(getAverageScore([2,3,45,6,2,2]));
 //function to check validation of score
 
 function isValidScore(score) {
- return typeof score === "number" && score >= 0 && score <= 100;
+  return typeof score === "number" && score >= 0 && score <= 100;
 }
 
 console.log(isValidScore(2));
@@ -74,9 +75,9 @@ console.log(getGrade(""));
 //function to check if student has a passing check
 
 function hasPassingGrade(score) {
-if(!isValidScore(score)) return "Invalid score";
-if (getGrade(score) !== "F") return true;
-else return false;
+  if(!isValidScore(score)) return "Invalid score";
+  if (getGrade(score) !== "F") return true;
+  else return false;
 }
 
 console.log(hasPassingGrade(999));
@@ -88,17 +89,17 @@ console.log(hasPassingGrade(60));
 //function to return a message 
 
 function studentMsg(totalScores, studentScore) {
-
+  
   const average = getAverageScore(totalScores); 
   const grade = getGrade(studentScore);
   const passed = hasPassingGrade(studentScore);
-
+  
   const  baseMessage = `Class average: ${average}. Your grade: ${grade}.`
-
+  
   if (!isValidScore(studentScore)) return "Invalid student score."
   if(hasPassingGrade(studentScore)) return baseMessage + " You passed the course."
   else return baseMessage + " You failed the course."
-
+  
   
 }
 
@@ -106,3 +107,27 @@ console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
 console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 60));
 console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 100));
 console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 999));
+
+const backgroundColorsArr = [
+  "#D6A99D",
+  "#FBF3D5",
+  "#8AA624", 
+  "#DEE8CE"
+];
+
+ 
+function getRandomIndex() {
+  let randomNumber = Math.floor((backgroundColorsArr.length * Math.random()));
+  console.log(randomNumber);
+  return randomNumber;
+} 
+
+
+function switchBtnAction() {
+  const color = backgroundColorsArr[getRandomIndex()];
+  console.log(color);
+  body.style.backgroundColor = color;
+}
+
+switchBtn.addEventListener("click", switchBtnAction);
+
